9205ece984c95e586932eca2c956b280134101fc
shabad-singh-sethi <sethi.shabad.19@gmail.com>
2026-03-25 22:19:35 +0530

    INTRODUCTION

---
+++ intro/main.py
@@ -0,0 +1,20 @@
+print("HELLO I AM YOUR ADVANCE AI SYSTEM")
+
+name = input("MAY I KNOW YOUR NAME : ")
+
+occupation = input("PLEASE TELL ME ABOUT YOUR OCCUPATION: ")
+
+print(f"WOW! {name}, SO YOU ARE A {occupation}")
+
+mood = input("HOWS YOUR MOOD TODAY ?: ").lower()
+
+if mood == "good":
+    print("GREAT!")
+
+elif mood == "bad":
+    print("SO SAD TO HEAR THAT")
+
+else:
+    print("INVALID")
+
+
